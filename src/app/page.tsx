a9fd663332350f66028ba34bca668bba038c4256
Agustin Lozano <agustin.lozano@itrocksweb.com>
2024-07-14 15:22:26 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{" "} agustin lozano
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
