5532ba919b659502e388d6a473886b794d2a7218
Agustin-Lozano <Agustin.Lozano@e-hps.com>
2024-11-09 11:38:38 -0300

    Completed

--- src/components/state/index.tsx
+++ src/components/state/index.tsx
@@ -1,7 +1,6 @@
 export default function State({ customStyling, text, onClick }: IState) {
   return (
     <p onClick={onClick} className={`${customStyling} cursor-pointer border-[1px] border-green-500 p-2 rounded-xl text-sm md:text-xl min-w-[105px] min-h-min flex justify-center items-center  h-min text-center font-bold`} >{text}</p>
-
   )
 }
 
